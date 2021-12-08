d10637fda93358a2da3689a7cec4a55a8cba6178
OkonoWil <okonowilfried@gmail.com>
2021-12-08 02:46:05 +0100

    La boîte de dialogue est prêt

--- Fenetre.java
+++ Fenetre.java
@@ -3,6 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 public class Fenetre extends JFrame {
     private JButton bouton = new JButton("Appel à la ZDialog");
@@ -16,13 +18,16 @@ public class Fenetre extends JFrame {
         this.getContentPane().add(bouton);
         bouton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent arg0) {
-                ZDialog zd = new ZDialog(null, "Coucou les ZérOs", true);
+                ZDialog zd = new ZDialog(null, "Coucou les Zeros", true);
+                ZDialogInfo zInfo = zd.showZDialog();
+                JOptionPane jop = new JOptionPane();
+                jop.showMessageDialog(null, zInfo.toString(), "Informations personnage", JOptionPane.INFORMATION_MESSAGE);
             }
         });
         this.setVisible(true);
     }
-
     public static void main(String[] main){
         Fenetre fen = new Fenetre();
     }
+    
 }
